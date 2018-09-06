d6a08924ffcf86421f8c62d78c9d3515df57e2c4
Abhay Tangella <abhaytangella@lawn-128-61-39-248.lawn.gatech.edu>
2018-09-06 15:21:34 -0400

    changed Person1

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,11 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+		String beginningTwo = input.substring(0, 2);
+		String restOfString = input.substring(2, input.length());
+		String changedString = restOfString + beginningTwo;
+
+	  return changedString;
 	}
 	
 	/**
